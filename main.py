8066c74272a3ceae1c7c48eb8b926a6e0da5c6f0
Brodie969 <131664136+Brodie969@users.noreply.github.com>
2023-08-07 11:15:09 +1000

    Removed the hardcoding of screen dimensions.
    The script now check for screen resolution before starting the script.

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import random
 import pyautogui
 import time
+import tkinter as tk
 
+root = tk.Tk()
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
 pyautogui.FAILSAFE = False
 
 for i in range(1200):
-    x = random.randint(1, 1920)
-    y = random.randint(1, 1080)
+    x = random.randint(0, screen_width)
+    y = random.randint(0, screen_height)
     pyautogui.moveTo(x, y)
     time.sleep(0.1)
